7105432654539e86fe054290c69905feaec05cd1
somofficial7504 <148617092+somofficial7504@users.noreply.github.com>
2026-02-26 15:57:47 +0530

    Update App.tsx
    Sandeep Done Changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50">
 
-      
+       <h1> Sandeep Sahu</h1>
       <h1 className="text-2xl font-semibold text-gray-800">fit2fly</h1>
     </main>
   );
